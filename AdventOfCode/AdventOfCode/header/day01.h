18b28f784f2222eaf85ef460e9119b55042933fc
Fco. Javier Fuentes Barragán <bbbfuentes@correo.ugr.es>
2019-12-04 19:43:37 +0100

    Generacion de la estructura inicial del proyecto

---
+++ AdventOfCode/AdventOfCode/header/day01.h
@@ -0,0 +1,14 @@
+#pragma once
+#ifndef DAYONE
+#define DAYONE
+
+
+
+#include <string>
+
+namespace DayOne 
+{
+	int calculateMass(std::string input);
+}
+
+#endif // !1
---
+++ AdventOfCode/AdventOfCode/source/day01.cpp
@@ -0,0 +1,8 @@
+
+
+#include "..\header\day01.h"
+
+int DayOne::calculateMass(std::string input)
+{
+	return 0;
+}
